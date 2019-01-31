1fdf56bd0894b88333c2ad84f26d70f81db6bec7
Laureen Schausberger <laureen.schausberger@gmx.at>
2019-01-31 14:16:03 +0000

    initial setup

---
+++ Ray/Ray/Data/DataHandler.swift
@@ -0,0 +1,13 @@
+//
+//  DataHandler.swift
+//  Ray
+//
+//  Created by Laureen Schausberger on 31.01.19.
+//  Copyright © 2019 Laureen Schausberger. All rights reserved.
+//
+
+import Foundation
+
+final class DataHandler {
+    
+}
---
+++ Ray/Ray/MainViewController.swift
@@ -0,0 +1,20 @@
+//
+//  MainViewController.swift
+//  Ray
+//
+//  Created by Laureen Schausberger on 31.01.19.
+//  Copyright © 2019 Laureen Schausberger. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
