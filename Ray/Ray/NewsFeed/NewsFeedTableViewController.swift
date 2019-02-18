b144cdcb2bdb6bb821a3aa596f8031f621198816
kirstybutler <kirstybutler6@gmail.com>
2019-02-18 12:53:50 +0000

    no message

---
+++ Ray/Ray/NewsFeed/NewsFeedTableViewController.swift
@@ -0,0 +1,9 @@
+//
+//  NewsFeedTableViewController.swift
+//  Ray
+//
+//  Created by Kirsty Samantha Butler on 17/02/2019.
+//  Copyright © 2019 Laureen Schausberger. All rights reserved.
+//
+
+import Foundation
---
+++ Ray/Ray/NewsFeed/XMLParser.swift
@@ -0,0 +1,15 @@
+//
+//  XMLParser.swift
+//  Ray
+//
+//  Created by Kirsty Samantha Butler on 16/02/2019.
+//  Copyright © 2019 Laureen Schausberger. All rights reserved.
+//
+
+import Foundation
+
+struct RSSItem {
+    var title: String
+    var date: String
+    var desc: String
+}
