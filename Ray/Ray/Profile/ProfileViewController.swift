d5d7c6c69504c5814e387bbf00b3df7f30dba628
kirstybutler <kirstybutler6@gmail.com>
2019-02-26 21:22:20 +0000

    MOD button appearance

--- Ray/Ray/Profile/ProfileViewController.swift
+++ Ray/Ray/Profile/ProfileViewController.swift
@@ -2,13 +2,13 @@
 //  ProfileViewController.swift
 //  Ray
 //
-//  Created by Kirsty Samantha Butler on 26/02/2019.
+//  Creat/Users/ksb36/ukc-client-project/Ray/Ray/Profile/ProfileViewController.swifted by Kirsty Samantha Butler on 26/02/2019.
 //  Copyright © 2019 Laureen Schausberger. All rights reserved.
 //
 
 import UIKit
 
-class ProfileViewController: UIViewController, UITextFieldDelegate {
+class ProfileViewController: UIViewController {
     
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
@@ -19,18 +19,19 @@ class ProfileViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet weak var addLocation: UIButton!
     
-    @IBAction func ukcBtn(_ sender: UIButton) {
-    }
-    @IBAction func addBtn(_ sender: UIButton) {
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        setupLayout()
+        addLocation.backgroundColor = .princetonOrange
+        addLocation.layer.cornerRadius = 5
+        
+    }
+    @IBAction func location(_ sender: Any) {
+        
     }
     
-    private func setupLayout(){
-        
+    @IBAction func ukcBtn(_ sender: UIButton) {
+    }
+    @IBAction func addBtn(_ sender: UIButton) {
     }
 }
