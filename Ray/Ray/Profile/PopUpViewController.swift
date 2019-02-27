879dac171e0ed276d39f7acc6b16c7ecc2b24f43
kirstybutler <kirstybutler6@gmail.com>
2019-02-27 22:05:41 +0000

    ADD PopUpViewController file

---
+++ Ray/Ray/Profile/PopUpViewController.swift
@@ -0,0 +1,16 @@
+//
+//  PopUpViewController.swift
+//  Ray
+//
+//  Created by Kirsty Samantha Butler on 27/02/2019.
+//  Copyright © 2019 Laureen Schausberger. All rights reserved.
+//
+
+import UIKit
+
+class PopUpViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
