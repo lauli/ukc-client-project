6aa7070cfb072c65016cb510eecab968493e9f72
Phoebe <pcehb@icloud.com>
2019-02-28 14:35:41 +0000

    ADD Reset Password page, ADD Create Account Page, ADD Background image, MOD Login Page, MOD Main storyboard for login pages

--- Ray/Ray/New Group/LoginViewController.swift
+++ Ray/Ray/New Group/LoginViewController.swift
@@ -16,19 +16,39 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let backgroundImage = UIImageView(frame: UIScreen.main.bounds)
+        backgroundImage.image = UIImage(named: "backgroundimage.png")
+        backgroundImage.contentMode = UIView.ContentMode.scaleAspectFill
+        self.view.insertSubview(backgroundImage, at: 0)
         // Do any additional setup after loading the view.
+        
     }
     
 
     @IBAction func loginBtnTapped(_ sender: Any) {
         guard let email = self.emailField.text, let password = self.passwordField.text else {
+            let alertController = UIAlertController(title: "Alert", message: "Email/Password incorrect.", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in
+                //OK Action
+            }))
+            self.present(alertController, animated: true, completion: nil)
             return
         }
-        
-        Auth.auth().signIn(withEmail: email, password: password) { [weak self] user, error in
-            guard let strongSelf = self else { return }
-            print("LOGIN")
+        Auth.auth().signIn(withEmail: email, password: password) { user, error in
+            if let error = error {
+                let alertController = UIAlertController(title: "Alert", message: error.localizedDescription, preferredStyle: .alert)
+                alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: { _ in
+                    //OK Action
+                }))
+                self.present(alertController, animated: true, completion: nil)
+                return
+            }
+            else{
+                 print("LOGIN")
+                
+            }
+            
+            
         }
 
         
